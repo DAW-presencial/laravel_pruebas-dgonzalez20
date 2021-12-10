220e3b801c706c53a975d7f2eac59e0024473877
david <dgonzalezl@cifpfbmoll.eu>
2021-12-10 16:26:54 +0100

    pruebas rutas

--- proyecto_laravel_2021/routes/web.php
+++ proyecto_laravel_2021/routes/web.php
@@ -1,18 +1,30 @@
 <?php
-
-use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
+//Ruta a la raíz del proyecto laravel
+Route::get('/',function (){
+ return "Haciendo una pruwba de ruta desde la raíz";
 });
+/*Ruta que accede a la web contactos si tenemos que hacer mas rutas especificas hay que cambiar
+las rutas en función del destino*/
+Route::get('contactos', function(){
+  return "Haciendo prueba desde la ruta desde la web contactos";
+
+});
+
+Route::post(); // en función del método que estamos usando en el formulario,
+Route::put();
+Route::path();
+Route::delete();
+
+//Pasar un parámetro a la ruta que ha ser cumplimentado de manera obligada sino error 404 Not Funt
+ Route::get('contactos/{nombre}',function ($nombre){
+    return "Prueba de ruta con parámetros" .$nombre;
+});
+
+ //Ruta con parámetro no obligatorio en caso de no pasar parámetro, lo detectara como invitado
+
+Route::get('contactos/{nombre?}',function ($nombre="Invitado"){
+    return "Prueba de ruta con parámetros" .$nombre;
+});
+
+//Rpute:get ('pruebas_laravel',function
+?>
