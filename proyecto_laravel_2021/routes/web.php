a85fc33754909c2fa3cc1438d318aa3ae3a965ac
david <dgonzalezl@cifpfbmoll.eu>
2022-02-06 11:27:42 +0100

    Subo ficheros vistas

--- proyecto_laravel_2021/routes/web.php
+++ proyecto_laravel_2021/routes/web.php
@@ -1,30 +1,7 @@
 <?php
-//Ruta a la raíz del proyecto laravel
-Route::get('/',function (){
- return "Haciendo una pruwba de ruta desde la raíz";
-});
-/*Ruta que accede a la web contactos si tenemos que hacer mas rutas especificas hay que cambiar
-las rutas en función del destino*/
-Route::get('contactos', function(){
-  return "Haciendo prueba desde la ruta desde la web contactos";
 
-});
+route::view('/',home)->name("home ");
+route::view('/',about)->name("about");
+route::view('/',portafolia)->name("portafolio");
+route::view('/',"contaacto")->name("contacto ");
 
-Route::post(); // en función del método que estamos usando en el formulario,
-Route::put();
-Route::path();
-Route::delete();
-
-//Pasar un parámetro a la ruta que ha ser cumplimentado de manera obligada sino error 404 Not Funt
- Route::get('contactos/{nombre}',function ($nombre){
-    return "Prueba de ruta con parámetros" .$nombre;
-});
-
- //Ruta con parámetro no obligatorio en caso de no pasar parámetro, lo detectara como invitado
-
-Route::get('contactos/{nombre?}',function ($nombre="Invitado"){
-    return "Prueba de ruta con parámetros" .$nombre;
-});
-
-//Rpute:get ('pruebas_laravel',function
-?>
